9989d75ecfce88749ff3eaabb687180256176cdf
Taiyou06 <kaandindar21@gmail.com>
2025-05-01 23:30:40 +0200

    [ci skip] remove experimental tags from playerdata and target finding

--- leaf-server/src/main/java/org/dreeam/leaf/config/modules/async/AsyncTargetFinding.java
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/async/AsyncTargetFinding.java
@@ -11,7 +11,6 @@ public class AsyncTargetFinding extends ConfigModules {
         return EnumConfigCategory.ASYNC.getBaseKeyName() + ".async-target-finding";
     }
 
-    @Experimental
     public static boolean enabled = false;
     public static boolean alertOther = true;
     public static boolean searchBlock = true;
@@ -22,11 +21,9 @@ public class AsyncTargetFinding extends ConfigModules {
     @Override
     public void onLoaded() {
         config.addCommentRegionBased(getBasePath(), """
-                **Experimental feature**
                 This moves the expensive entity and block search calculations to background thread while
                 keeping the actual validation on the main thread.""",
             """
-                **实验性功能**
                 这会将昂贵的实体目标搜索计算移至后台线程, 同时在主线程上保持实际的实体验证.""");
 
         if (asyncTargetFindingInitialized) {
