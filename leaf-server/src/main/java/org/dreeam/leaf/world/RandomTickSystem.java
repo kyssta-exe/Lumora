6ffd1fd5d30c7311287a7edc65052237ee6d5f57
hayanesuru <hayanesuru@outlook.jp>
2025-06-29 23:58:26 +0900

    backport from dev/1.21.6
    73ffcb09fa9a846b15688001714a662c1d5325c7 optimize mob spawning
    333373d2048df32b14b6d0e87dc6d0ee55c810ee fix async mob spawning data race
    7c9f88e4f84063463530fb276f779b6776ad6132 [ci skip] cleanup
    bf9486f0f096dc56f82943cf6d1c169d9d13cb54 remove hash lookup in optimize random tick
    915ac01cd374b39622eb8d996b2aa94943a11fbb cleanup
    b90c1cd527d2fbd708845b1326eae18e9d988a29 fix playermobcaps command
    fd34d9f62609d9f24b432172c554cc1cdc70fd9b cleanup
    5d663b4d36750b60f2c4ca550d76f7b0fd06fe83 optimize collectSpawningChunks (#382)

--- leaf-server/src/main/java/org/dreeam/leaf/world/RandomTickSystem.java
+++ leaf-server/src/main/java/org/dreeam/leaf/world/RandomTickSystem.java
@@ -1,115 +1,162 @@
 package org.dreeam.leaf.world;
 
+import ca.spottedleaf.moonrise.common.list.ReferenceList;
+import ca.spottedleaf.moonrise.common.list.ShortList;
 import it.unimi.dsi.fastutil.longs.LongArrayList;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.chunk.LevelChunkSection;
+import net.minecraft.world.level.levelgen.BitRandomSource;
 import net.minecraft.world.level.material.FluidState;
-
-import java.util.OptionalLong;
 
 public final class RandomTickSystem {
     private static final long SCALE = 0x100000L;
-    private static final long CHUNK_BLOCKS = 4096L;
-
-    /// reduce unnecessary sampling and block counting
-    private static final long TICK_MASK = 0b11L;
-    private static final long TICK_MUL = 4L;
+    private static final long TICK_FILTER_MASK = 0b11L;
+    private static final long CHUNK_BLOCKS = 4096L / 4L;
     private static final int BITS_STEP = 2;
     private static final int BITS_MAX = 60;
 
     private final LongArrayList queue = new LongArrayList();
     private final LongArrayList samples = new LongArrayList();
     private final LongArrayList weights = new LongArrayList();
-    private long weightsSum = 0L;
-
-    private int bits = 60;
-    private long cacheRandom = 0L;
 
     public void tick(ServerLevel world) {
-        if (weights.isEmpty() || samples.isEmpty()) {
+        queue.clear();
+        samples.clear();
+        weights.clear();
+
+        final BitRandomSource random = world.simpleRandom;
+        final ReferenceList<LevelChunk> entityTickingChunks = world.moonrise$getEntityTickingChunks();
+        final int randomTickSpeed = world.getGameRules().getInt(GameRules.RULE_RANDOMTICKING);
+        final LevelChunk[] raw = entityTickingChunks.getRawDataUnchecked();
+        final int size = entityTickingChunks.size();
+        final boolean disableIceAndSnow = world.paperConfig().environment.disableIceAndSnow;
+        if (randomTickSpeed <= 0) {
             return;
         }
-
-        final var random = world.simpleRandom;
-        final long chosen;
-        if (((weightsSum % SCALE) >= boundedNextLong(random, SCALE))) {
-            chosen = weightsSum / SCALE + 1L;
-        } else {
-            chosen = weightsSum / SCALE;
+        if (!disableIceAndSnow) {
+            iceSnow(world, size, randomTickSpeed, random, raw);
         }
+        final long weightsSum = collectTickingChunks(size, random, raw, randomTickSpeed);
+        if (samples.isEmpty() || weightsSum == 0L) {
+            return;
+        }
+        sampling(random, weightsSum);
+
+        final long[] q = queue.elements();
+        final int minY = ca.spottedleaf.moonrise.common.util.WorldUtil.getMinSection(world) << 4;
+        for (int k = 0, len = queue.size(); k < len; ++k) {
+            final long packed = q[k];
+            final LevelChunk chunk = raw[(int) (packed >>> 16)];
+            tickBlock(world, chunk, (int) (packed & 0xFFFF), random, minY);
+        }
+    }
+
+    private void sampling(BitRandomSource random, long weightsSum) {
+        final long chosen = ((weightsSum % SCALE) >= boundedNextLong(random, SCALE))
+            ? (weightsSum / SCALE + 1L)
+            : (weightsSum / SCALE);
         if (chosen == 0L) {
             return;
         }
 
+        final long[] w = weights.elements();
+        final long[] s = samples.elements();
+        long accumulated = w[0];
         final long spoke = weightsSum / chosen;
-        if (spoke == 0L) {
-            return;
-        }
+        if (spoke == 0L) return;
 
-        final long[] weightsRaw = weights.elements();
-        final long[] samplesRaw = samples.elements();
-
-        long accumulated = weightsRaw[0];
         long current = boundedNextLong(random, spoke);
         int i = 0;
         while (current < weightsSum) {
             while (accumulated < current) {
-                i += 1;
-                accumulated += weightsRaw[i];
+                i++;
+                accumulated += w[i];
             }
-            queue.add(samplesRaw[i]);
+            queue.add(s[i]);
             current += spoke;
         }
-        while (queue.size() < chosen) {
-            queue.add(samplesRaw[i]);
-        }
-
-        long[] queueRaw = queue.elements();
-        int j = 0;
-        int k;
-        for (k = queue.size() - 3; j < k; j += 4) {
-            final long packed1 = queueRaw[j];
-            final long packed2 = queueRaw[j + 1];
-            final long packed3 = queueRaw[j + 2];
-            final long packed4 = queueRaw[j + 3];
-            final LevelChunk chunk1 = getChunk(world, packed1);
-            final LevelChunk chunk2 = packed1 != packed2 ? getChunk(world, packed2) : chunk1;
-            final LevelChunk chunk3 = packed2 != packed3 ? getChunk(world, packed3) : chunk2;
-            final LevelChunk chunk4 = packed3 != packed4 ? getChunk(world, packed4) : chunk3;
-            if (chunk1 != null) tickBlock(world, chunk1, random);
-            if (chunk2 != null) tickBlock(world, chunk2, random);
-            if (chunk3 != null) tickBlock(world, chunk3, random);
-            if (chunk4 != null) tickBlock(world, chunk4, random);
-        }
-        for (k = queue.size(); j < k; j++) {
-            final LevelChunk chunk = getChunk(world, queueRaw[j]);
-            if (chunk != null) tickBlock(world, chunk, random);
-        }
-
-        weightsSum = 0L;
-        queue.clear();
-        weights.clear();
-        samples.clear();
     }
 
-    private static LevelChunk getChunk(ServerLevel world, long packed) {
-        return world.chunkSource.getChunkAtIfLoadedImmediately((int) packed, (int) (packed >> 32));
+    private long collectTickingChunks(int size, BitRandomSource random, LevelChunk[] raw, long randomTickSpeed) {
+        int bits = 0;
+        long cacheRandom = random.nextLong();
+        long weightsSum = 0L;
+
+        for (int i = 0; i < size; i++) {
+            if (bits != BITS_MAX) {
+                bits += BITS_STEP;
+            } else {
+                bits = 0;
+                cacheRandom = random.nextLong();
+            }
+            if ((cacheRandom & (TICK_FILTER_MASK << bits)) != 0L) {
+                continue;
+            }
+            final LevelChunk chunk = raw[i];
+            if (chunk.leaf$tickingBlocksDirty) {
+                populateChunkTickingCount(chunk);
+            }
+            int[] data = chunk.leaf$tickingCount;
+            for (int packed : data) {
+                int count = packed >>> 16;
+                int idx = packed & 0xFFFF;
+                samples.add((((long) i) << 16 | idx));
+                long weight = (randomTickSpeed * count * SCALE) / CHUNK_BLOCKS;
+                weights.add(weight);
+                weightsSum += weight;
+            }
+        }
+        return weightsSum;
     }
 
-    private static void tickBlock(ServerLevel world, LevelChunk chunk, RandomSource random) {
-        int count = chunk.leaf$tickingBlocksCount();
-        if (count == 0) {
-            return;
+    private static void populateChunkTickingCount(LevelChunk chunk) {
+        chunk.leaf$tickingBlocksDirty = false;
+        int sum = 0;
+        for (LevelChunkSection section : chunk.getSections()) {
+            sum += (section.moonrise$getTickingBlockList().size() == 0) ? 0 : 1;
         }
-        OptionalLong optionalPos = chunk.leaf$getTickingPos(random.nextInt(count));
-        if (optionalPos.isEmpty()) {
-            return;
+
+        if (chunk.leaf$tickingCount.length != sum) {
+            chunk.leaf$tickingCount = new int[sum];
         }
-        BlockPos pos = BlockPos.of(optionalPos.getAsLong());
-        BlockState state = chunk.getBlockStateFinal(pos.getX(), pos.getY(), pos.getZ());
+
+        int k = 0;
+        LevelChunkSection[] sections = chunk.getSections();
+        for (int j = 0; j < sections.length; j++) {
+            ShortList list = sections[j].moonrise$getTickingBlockList();
+            int n = list.size();
+            if (n != 0) {
+                chunk.leaf$tickingCount[k++] = (n << 16) | (j & 0xFFFF);
+            }
+        }
+    }
+
+    private static void iceSnow(ServerLevel world, int size, int randomTickSpeed, BitRandomSource random, LevelChunk[] raw) {
+        int currentIceAndSnowTick = random.nextInt(48 * 16);
+        for (int i = 0; i < size; i++) {
+            currentIceAndSnowTick -= randomTickSpeed;
+            if (currentIceAndSnowTick <= 0) {
+                currentIceAndSnowTick = random.nextInt(48 * 16);
+                LevelChunk chunk = raw[i];
+                ChunkPos pos = chunk.getPos();
+                world.tickPrecipitation(world.getBlockRandomPos(pos.getMinBlockX(), 0, pos.getMinBlockZ(), 15));
+            }
+        }
+    }
+
+    private static void tickBlock(ServerLevel world, LevelChunk chunk, int sectionIdx, BitRandomSource random, int minSection) {
+        LevelChunkSection section = chunk.getSection(sectionIdx);
+        ShortList list = section.moonrise$getTickingBlockList();
+        int size = list.size();
+        if (size == 0) return;
+        short location = list.getRaw(boundedNextInt(random, size));
+        BlockState state = section.states.get(location);
+        final BlockPos pos = new BlockPos((location & 15) | (chunk.locX << 4), (location >>> 8) | (minSection + (sectionIdx << 4)), ((location >>> 4) & 15) | (chunk.locZ << 4));
         state.randomTick(world, pos, random);
 
         final boolean doubleTickFluids = !ca.spottedleaf.moonrise.common.PlatformHooks.get().configFixMC224294();
@@ -121,46 +168,31 @@ public final class RandomTickSystem {
         }
     }
 
-    public void tickChunk(
-        RandomSource random,
-        LevelChunk chunk,
-        long tickSpeed
-    ) {
-        if (this.bits == BITS_MAX) {
-            this.bits = 0;
-            this.cacheRandom = random.nextLong();
-        } else {
-            this.bits += BITS_STEP;
-        }
-        if ((this.cacheRandom & (TICK_MASK << bits)) == 0L) {
-            long count = chunk.leaf$tickingBlocksCount();
-            if (count != 0L) {
-                long weight = (TICK_MUL * tickSpeed * count * SCALE) / CHUNK_BLOCKS;
-                samples.add(chunk.getPos().longKey);
-                weights.add(weight);
-                weightsSum += weight;
-            }
-        }
-    }
-
-    /**
-     * @param rng a random number generator to be used as a
-     *        source of pseudorandom {@code long} values
-     * @param bound the upper bound (exclusive); must be greater than zero
-     *
-     * @return a pseudorandomly chosen {@code long} value
-     *
-     * @see java.util.random.RandomGenerator#nextLong(long) nextLong(bound)
-     */
-    public static long boundedNextLong(RandomSource rng, long bound) {
-        final long m = bound - 1;
+    private static long boundedNextLong(BitRandomSource rng, long bound) {
+        final long m = bound - 1L;
         long r = rng.nextLong();
         if ((bound & m) == 0L) {
             r &= m;
         } else {
+            //noinspection StatementWithEmptyBody
             for (long u = r >>> 1;
                  u + m - (r = u % bound) < 0L;
                  u = rng.nextLong() >>> 1)
+                ;
+        }
+        return r;
+    }
+
+    private static int boundedNextInt(BitRandomSource rng, int bound) {
+        final int m = bound - 1;
+        int r = rng.nextInt();
+        if ((bound & m) == 0) {
+            r &= m;
+        } else {
+            //noinspection StatementWithEmptyBody
+            for (int u = r >>> 1;
+                 u + m - (r = u % bound) < 0;
+                 u = rng.nextInt() >>> 1)
                 ;
         }
         return r;
