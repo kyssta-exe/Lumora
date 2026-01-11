d62782be30069b1fb08e0e6b08b91504447b01f7
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2026-01-11 18:19:12 -0500

    Some fixes

--- leaf-server/src/main/java/org/dreeam/leaf/util/LeafConstants.java
+++ leaf-server/src/main/java/org/dreeam/leaf/util/LeafConstants.java
@@ -4,4 +4,5 @@ public class LeafConstants {
 
     public static final boolean DISABLE_VANILLA_PROFILER = Boolean.getBoolean("leaf.disable-vanilla-profiler");
     public static final boolean ENABLE_FMA = Boolean.getBoolean("Leaf.enableFMA");
+    public static final boolean ENABLE_IO_URING = Boolean.getBoolean("Leaf.enable-io-uring");
 }
